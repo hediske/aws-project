23e92ea352fdd35aa3855e31855fc2330fcab17a
hediske <mohamedhedi.aissi@yahoo.fr>
2024-12-27 17:53:03 +0100

    created the source code for the App

---
+++ backend/TaskModel.js
@@ -0,0 +1,34 @@
+import { sequelize } from "./DBconfig.js";
+import { DataTypes } from 'sequelize';
+
+
+const Task = sequelize.define('Tasks', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    Startdate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    Enddate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    bannerUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+});
+Task.sync()
+export default Task;
